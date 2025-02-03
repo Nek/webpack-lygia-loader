783575c9c702e4468e3db4260f97d7535a34d33f
Nikita Dudnik (nek) <nikdudnik@gmail.com>
2025-02-03 19:35:33 +0100

    refactor: Replace @shaderfrog/glsl-parser with tree-sitter-glsl parser

--- src/parser.ts
+++ src/parser.ts
@@ -1,9 +1,15 @@
 import { LoaderContext } from 'webpack';
-import { parser, generate } from '@shaderfrog/glsl-parser';
-import { type DeclarationNode, visit, type NodeVisitors, type Path, type DeclarationStatementNode, DeclaratorListNode, FullySpecifiedTypeNode, KeywordNode, IdentifierNode, QualifierDeclaratorNode, TypeSpecifierNode, DefaultCaseNode, FunctionNode, PreprocessorNode, StructDeclarationNode, StructNode, LiteralNode } from '@shaderfrog/glsl-parser/ast/index.js';
+// import { parser, generate } from '@shaderfrog/glsl-parser';
+// import { type DeclarationNode, visit, type NodeVisitors, type Path, type DeclarationStatementNode, DeclaratorListNode, FullySpecifiedTypeNode, KeywordNode, IdentifierNode, QualifierDeclaratorNode, TypeSpecifierNode, DefaultCaseNode, FunctionNode, PreprocessorNode, StructDeclarationNode, StructNode, LiteralNode } from '@shaderfrog/glsl-parser/ast/index.js';
 import { writeFileSync } from 'fs';
 import path from 'path';
-import { match, P } from 'ts-pattern';
+// import { match, P } from 'ts-pattern';
+
+import Parser from 'tree-sitter';
+import GLSL from 'tree-sitter-glsl';
+
+const parser = new Parser();
+parser.setLanguage(GLSL as Parser.Language);
 
 
 const defaultValue: Record<string, Primitive> = {
@@ -26,68 +32,69 @@ type Uniforms = Record<string, Uniform>;
 const structs: Record<string, Struct> = {};
 const uniforms: Uniforms = {};
 
-const visitors: NodeVisitors = {
-    struct: {
-        enter: (node: Path<StructNode>) => {
-            console.log(node);
-            node.skip();
-        }
-    },
-    declaration_statement: {
-        enter: (node: Path<DeclarationStatementNode>) => {
-            const declaration = node.node.declaration as DeclaratorListNode;
-            const specified_type = declaration.specified_type as FullySpecifiedTypeNode;
-            const qualifier = (specified_type?.qualifiers?.[0] as KeywordNode)?.token;
-            if (qualifier !== 'uniform') {
-                const type = specified_type?.specifier?.specifier.type;
-                node.skip();
-                if (type === 'struct') {
-                    console.log(node.node);
-                    const specifier = (specified_type?.specifier?.specifier as StructNode).declarations[0].declaration.specified_type.specifier.token;
-                    const identifier = declaration.declarations?.[0].identifier.identifier;
-                    console.log(qualifier, specifier, identifier);
-                    // TODO: Handle structs
-                } else {
-                    node.remove();
-                }
-            } else {
-                const specifier = (specified_type.specifier.specifier as KeywordNode).token;
-                const identifier = declaration.declarations?.[0].identifier.identifier;
-                console.log(qualifier, specifier, identifier);
-                uniforms[identifier] = { value: defaultValue[specifier] };
-                node.skip();
-            }
-        }
-    },
-    preprocessor: {
-        enter: (node: Path<PreprocessorNode>) => {
-            node.remove();
-            node.skip();
-        }
-    },
-    function: {
-        enter: (node: Path<FunctionNode>) => {
-            node.remove();
-            node.skip();
-        }
-    },
-    default_case: {
-        enter: (node: Path<DefaultCaseNode>) => {
-            node.remove();
-            node.skip();
-        }
-    }
-}
+// const visitors: NodeVisitors = {
+//     struct: {
+//         enter: (node: Path<StructNode>) => {
+//             console.log(node);
+//             node.skip();
+//         }
+//     },
+//     declaration_statement: {
+//         enter: (node: Path<DeclarationStatementNode>) => {
+//             const declaration = node.node.declaration as DeclaratorListNode;
+//             const specified_type = declaration.specified_type as FullySpecifiedTypeNode;
+//             const qualifier = (specified_type?.qualifiers?.[0] as KeywordNode)?.token;
+//             if (qualifier !== 'uniform') {
+//                 const type = specified_type?.specifier?.specifier.type;
+//                 node.skip();
+//                 if (type === 'struct') {
+//                     console.log(node.node);
+//                     const specifier = (specified_type?.specifier?.specifier as StructNode).declarations[0].declaration.specified_type.specifier.token;
+//                     const identifier = declaration.declarations?.[0].identifier.identifier;
+//                     console.log(qualifier, specifier, identifier);
+//                     // TODO: Handle structs
+//                 } else {
+//                     node.remove();
+//                 }
+//             } else {
+//                 const specifier = (specified_type.specifier.specifier as KeywordNode).token;
+//                 const identifier = declaration.declarations?.[0].identifier.identifier;
+//                 console.log(qualifier, specifier, identifier);
+//                 uniforms[identifier] = { value: defaultValue[specifier] };
+//                 node.skip();
+//             }
+//         }
+//     },
+//     preprocessor: {
+//         enter: (node: Path<PreprocessorNode>) => {
+//             node.remove();
+//             node.skip();
+//         }
+//     },
+//     function: {
+//         enter: (node: Path<FunctionNode>) => {
+//             node.remove();
+//             node.skip();
+//         }
+//     },
+//     default_case: {
+//         enter: (node: Path<DefaultCaseNode>) => {
+//             node.remove();
+//             node.skip();
+//         }
+//     }
+// }
 
 export default function (this: LoaderContext<{}>, source: string) {
   const callback = this.async();
   try {
-    const ast = parser.parse(source);
-    visit(ast, visitors);
-    console.log(uniforms);
-    writeFileSync(`${path.basename(this.resourcePath)}.json`, JSON.stringify(ast, null, 2));
-    const generated = generate(ast);
-    writeFileSync(`transformed.${path.basename(this.resourcePath)}`, generated);
+    const tree = parser.parse(source);
+    const walker = tree.walk();
+    console.log(walker.currentNode);
+    // Implement depth first tree traversal with logging of a node's name AI!
+    
+
+    // console.log(tree.rootNode.toString());
     callback(null, `export default \`${source}\`;`);
   } catch (err: any) {
     callback(err);
